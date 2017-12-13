0066fabe0e8a91a5a2b1f790ffbdd28e2748f0b3
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-13 13:11:29 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update buble from v0.15.x to v0.18.x
    * update ESLint from v3 to v4
    * update eslint-config from v3 to v5, including an epic change https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/*!
- * metalsmith-babel | MIT (c) Shinnosuke Watanabe
- * https://github.com/babel/metalsmith-babel
-*/
 'use strict';
 
 const path = require('path');
@@ -12,50 +8,48 @@ const sourceMapToComment = require('source-map-to-comment');
 const toFastProperties = require('to-fast-properties');
 
 module.exports = function metalsmithBuble(options) {
-  options = Object.assign({sourceMap: false}, options);
+	options = Object.assign({sourceMap: false}, options);
 
-  if (typeof options.sourceMap !== 'boolean' && options.sourceMap !== 'inline') {
-    throw new TypeError(
-      '`sourceMap` option must be true, false or \'inline\', but got ' +
-      util.inspect(options.sourceMap) +
-      '.'
-    );
-  }
+	if (typeof options.sourceMap !== 'boolean' && options.sourceMap !== 'inline') {
+		throw new TypeError(`\`sourceMap\` option must be true, false or 'inline', but got ${
+			util.inspect(options.sourceMap)
+		}.`);
+	}
 
-  return function metalsmithBublePlugin(files, metalsmith) {
-    Object.keys(files).forEach(originalFilename => {
-      const ext = path.extname(originalFilename).toLowerCase();
-      if (ext !== '.js' && ext !== '.jsx') {
-        return;
-      }
+	return function metalsmithBublePlugin(files, metalsmith) {
+		Object.keys(files).forEach(originalFilename => {
+			const ext = path.extname(originalFilename).toLowerCase();
+			if (ext !== '.js' && ext !== '.jsx') {
+				return;
+			}
 
-      const filename = originalFilename.replace(/\.jsx$/i, '.js');
+			const filename = originalFilename.replace(/\.jsx$/i, '.js');
 
-      if (originalFilename !== filename) {
-        files[filename] = files[originalFilename];
-        delete files[originalFilename];
-        toFastProperties(files);
-      }
+			if (originalFilename !== filename) {
+				files[filename] = files[originalFilename];
+				delete files[originalFilename];
+				toFastProperties(files);
+			}
 
-      const result = buble.transform(files[filename].contents.toString(), Object.assign({}, options, {
-        source: path.join(metalsmith.directory(), metalsmith.source(), originalFilename),
-        file: path.join(metalsmith.directory(), metalsmith.destination(), filename)
-      }));
+			const result = buble.transform(files[filename].contents.toString(), Object.assign({}, options, {
+				source: path.join(metalsmith.directory(), metalsmith.source(), originalFilename),
+				file: path.join(metalsmith.directory(), metalsmith.destination(), filename)
+			}));
 
-      if (options.sourceMap === true) {
-        const sourcemapPath = `${filename}.map`;
-        files[sourcemapPath] = {
-          contents: new Buffer(JSON.stringify(result.map))
-        };
+			if (options.sourceMap === true) {
+				const sourcemapPath = `${filename}.map`;
+				files[sourcemapPath] = {
+					contents: new Buffer(JSON.stringify(result.map))
+				};
 
-        result.code += `\n//# sourceMappingURL=${
-          path.relative(path.dirname(filename), sourcemapPath).replace(/\\/g, '/')
-        }\n`;
-      } else if (options.sourceMap === 'inline') {
-        result.code += `\n${sourceMapToComment(result.map)}`;
-      }
+				result.code += `\n//# sourceMappingURL=${
+					path.relative(path.dirname(filename), sourcemapPath).replace(/\\/g, '/')
+				}\n`;
+			} else if (options.sourceMap === 'inline') {
+				result.code += `\n${sourceMapToComment(result.map)}`;
+			}
 
-      files[filename].contents = new Buffer(result.code);
-    });
-  };
+			files[filename].contents = new Buffer(result.code);
+		});
+	};
 };
